cb51eb36400974efd75d8840cd502da6275ba18a
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:22:14 +0800

    Add project files.

---
+++ Heritage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Heritage
+{
+    class hello
+    {
+        public string name;
+            public void Getdata(string name)
+        {
+            this.name = name;
+            Console.WriteLine("hello "+this.name);
+        }
+    }
+    class hello2 : hello
+    {
+        public void data() { 
+        
+        }
+    }
+     class program
+    {
+        static void Main(string[] arg)
+        {
+            hello2 obj1 = new hello2();
+            obj1.Getdata("aaa");
+           
+        }
+    }
+
+}
